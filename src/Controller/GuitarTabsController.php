ca080a837801a8eeca54fa16b5f41e8ca4a87c55
jakob-rzeppa <rzeppa.jakob@gmail.com>
2025-01-17 10:12:15 +0100

    add DAMA Doctrine Test Bundle configuration and create GuitarTabsController / tests

--- src/Controller/GuitarTabsController.php
+++ src/Controller/GuitarTabsController.php
@@ -2,57 +2,101 @@
 
 namespace App\Controller;
 
+use App\Entity\GuitarTab;
+use App\Repository\GuitarTabRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 final class GuitarTabsController extends AbstractController
 {
-    #[Route('/guitar/tabs', name: 'app_guitar_tabs_read_all', methods: ['GET'])]
-    public function readAll(): JsonResponse
+    #[Route('/guitar_tabs', name: 'app_guitar_tabs_read_all', methods: ['GET'])]
+    public function readAll(GuitarTabRepository $guitarTabRepository): JsonResponse
     {
+        $guitarTabs = $guitarTabRepository->findAll();
+
         return $this->json([
-            'message' => 'Welcome to your new controller!',
-            'path' => 'src/Controller/GuitarTabsController.php',
+            'data' => array_map(fn($guitarTab) => [
+                'id' => $guitarTab->getId(),
+                'name' => $guitarTab->getName(),
+                'artist' => $guitarTab->getArtist(),
+                'uploaded_at' => $guitarTab->getUploadedAt(),
+            ], $guitarTabs),
         ]);
     }
 
-    #[Route('/guitar/tabs/{id}', name: 'app_guitar_tab_read', methods: ['GET'])]
-    public function read(int $id): JsonResponse
+    #[Route('/guitar_tabs/{id}', name: 'app_guitar_tab_read', methods: ['GET'])]
+    public function read(int $id, GuitarTabRepository $guitarTabRepository): JsonResponse
     {
+        $guitarTab = $guitarTabRepository->find($id);
+
         return $this->json([
-            'message' => 'Welcome to your new controller!',
-            'path' => 'src/Controller/GuitarTabsController.php',
-            'id' => $id,
+            'id' => $guitarTab->getId(),
+            'name' => $guitarTab->getName(),
+            'artist' => $guitarTab->getArtist(),
+            'capo' => $guitarTab->getCapo(),
+            'uploaded_at' => $guitarTab->getUploadedAt(),
+            'content' => $guitarTab->getContent(),
         ]);
     }
 
-    #[Route('/guitar/tabs', name: 'app_guitar_tabs_create', methods: ['POST'])]
-    public function create(): JsonResponse
+    #[Route('/guitar_tabs', name: 'app_guitar_tabs_create', methods: ['POST'])]
+    public function create(Request $request, EntityManagerInterface $entityManagerInterface): JsonResponse
     {
+        $body = $request->toArray();
+
+        $guitarTab = new GuitarTab();
+        $guitarTab->setName($body['name']);
+        $guitarTab->setArtist($body['artist']);
+        $guitarTab->setCapo($body['capo']);
+        $guitarTab->setUploadedAt(new \DateTimeImmutable());
+        $guitarTab->setContent($body['content']);
+
+        $entityManagerInterface->persist($guitarTab);
+        $entityManagerInterface->flush();
+
         return $this->json([
-            'message' => 'Welcome to your new controller!',
-            'path' => 'src/Controller/GuitarTabsController.php',
-        ]);
+            'id' => $guitarTab->getId(),
+        ], 201);
     }
 
-    #[Route('/guitar/tabs/{id}', name: 'app_guitar_tabs_update', methods: ['PUT'])]
-    public function update(int $id): JsonResponse
+    #[Route('/guitar_tabs/{id}', name: 'app_guitar_tabs_update', methods: ['PUT'])]
+    public function update(int $id, Request $request, GuitarTabRepository $guitarTabRepository, EntityManagerInterface $entityManagerInterface): Response
     {
-        return $this->json([
-            'message' => 'Welcome to your new controller!',
-            'path' => 'src/Controller/GuitarTabsController.php',
-            'id' => $id,
-        ]);
+        $body = $request->toArray();
+
+        $guitarTab = $guitarTabRepository->find($id);
+
+        if (!$guitarTab) {
+            return new JsonResponse(['error' => 'Guitar tab not found'], 404);
+        }
+
+        $guitarTab->setName($body['name'] ?? $guitarTab->getName());
+        $guitarTab->setArtist($body['artist'] ?? $guitarTab->getArtist());
+        $guitarTab->setCapo($body['capo'] ?? $guitarTab->getCapo());
+        $guitarTab->setContent($body['content'] ?? $guitarTab->getContent());
+        $entityManagerInterface->persist($guitarTab);
+
+        $entityManagerInterface->flush();
+
+        return new Response('', 204);
     }
 
-    #[Route('/guitar/tabs/{id}', name: 'app_guitar_tabs_delete', methods: ['DELETE'])]
-    public function delete(int $id): JsonResponse
+    #[Route('/guitar_tabs/{id}', name: 'app_guitar_tabs_delete', methods: ['DELETE'])]
+    public function delete(int $id, GuitarTabRepository $guitarTabRepository, EntityManagerInterface $entityManagerInterface): Response
     {
-        return $this->json([
-            'message' => 'Welcome to your new controller!',
-            'path' => 'src/Controller/GuitarTabsController.php',
-            'id' => $id,
-        ]);
+        $guitarTab = $guitarTabRepository->find($id);
+
+        if (!$guitarTab) {
+            return new JsonResponse(['error' => 'Guitar tab not found'], 404);
+        }
+
+        $entityManagerInterface->remove($guitarTab);
+        $entityManagerInterface->flush();
+
+        return new Response('', 204);
     }
 }
--- tests/Controller/GuitarTabsControllerTest.php
+++ tests/Controller/GuitarTabsControllerTest.php
@@ -6,11 +6,105 @@ use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
 final class GuitarTabsControllerTest extends WebTestCase
 {
-    public function testIndex(): void
+    public function testReadAll(): void
     {
+        $expected = [
+            'data' => [
+                [
+                    'id' => 1,
+                    'name' => 'Hotel California',
+                    'artist' => 'Eagles',
+                    'uploaded_at' => new \DateTimeImmutable('2021-01-01'),
+                ],
+            ],
+        ];
+
         $client = static::createClient();
-        $client->request('GET', '/guitar/tabs');
+        $client->request('GET', '/guitar_tabs');
 
         self::assertResponseIsSuccessful();
+        self::assertJson($client->getResponse()->getContent());
+        self::assertJsonStringEqualsJsonString(json_encode($expected), $client->getResponse()->getContent());
+    }
+
+    public function testRead(): void
+    {
+        $expected = [
+            'id' => 1,
+            'name' => 'Hotel California',
+            'artist' => 'Eagles',
+            'capo' => 7,
+            'uploaded_at' => new \DateTimeImmutable('2021-01-01'),
+            'content' => "\\n[Verse]\\nBm F# A E G D Em F#\\nBm F# A E G D Em F#\\nBm F# A E G D Em F#\\nBm F# A E G D Em F#\\n[Chorus]\\nG D F# Bm G D Em F#\\nG D F# Bm G D Em F#\\nG D F# Bm G D Em F#\\nG D F# Bm G D Em F#",
+        ];
+
+        $client = static::createClient();
+        $client->request('GET', '/guitar_tabs/1');
+
+        self::assertResponseIsSuccessful();
+        self::assertJson($client->getResponse()->getContent());
+        self::assertJsonStringEqualsJsonString(json_encode($expected), $client->getResponse()->getContent());
+    }
+
+    public function testCreate(): void
+    {
+        $expected = [
+            'id' => 2,
+        ];
+
+        $client = static::createClient();
+        $client->request('POST', '/guitar_tabs', [], [], [], json_encode([
+            'name' => 'Hotel California',
+            'artist' => 'Eagles',
+            'capo' => 7,
+            'content' => "\n[Verse]\nBm F# A E G D Em F#\nBm F# A E G D Em F#\nBm F# A E G D Em F#\nBm F# A E G D Em F#\n[Chorus]\nG D F# Bm G D Em F#\nG D F# Bm G D Em F#\nG D F# Bm G D Em F#\nG D F# Bm G D Em F#",
+        ]));
+
+        self::assertResponseIsSuccessful();
+        self::assertResponseStatusCodeSame(201, $client->getResponse()->getStatusCode());
+        self::assertJson($client->getResponse()->getContent());
+        self::assertJsonStringEqualsJsonString(json_encode($expected), $client->getResponse()->getContent());
+    }
+
+    public function testUpdate(): void
+    {
+        $client = static::createClient();
+        $client->request('PUT', '/guitar_tabs/1', [], [], [], json_encode([
+            'name' => 'new',
+            'artist' => 'new',
+            'capo' => 8,
+            'content' => "new",
+        ]));
+
+        self::assertResponseStatusCodeSame(204, $client->getResponse()->getStatusCode());
+    }
+
+    public function testUpdateNotFound(): void
+    {
+        $client = static::createClient();
+        $client->request('PUT', '/guitar_tabs/100', [], [], [], json_encode([
+            'name' => 'new',
+            'artist' => 'new',
+            'capo' => 8,
+            'content' => "new",
+        ]));
+
+        self::assertResponseStatusCodeSame(404, $client->getResponse()->getStatusCode());
+    }
+
+    public function testDelete(): void
+    {
+        $client = static::createClient();
+        $client->request('DELETE', '/guitar_tabs/1');
+
+        self::assertResponseStatusCodeSame(204, $client->getResponse()->getStatusCode());
+    }
+
+    public function testDeleteNotFound(): void
+    {
+        $client = static::createClient();
+        $client->request('DELETE', '/guitar_tabs/100');
+
+        self::assertResponseStatusCodeSame(404, $client->getResponse()->getStatusCode());
     }
 }
