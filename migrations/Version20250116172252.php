0058c841fa8161da3e4dbb5949ae6f8397b99948
jakob-rzeppa <rzeppa.jakob@gmail.com>
2025-01-19 10:52:14 +0100

    auto-generate security, user (entity, migration, fixtures)

--- migrations/Version20250116172252.php
+++ migrations/Version20250116172252.php
@@ -19,10 +19,10 @@ final class Version20250116172252 extends AbstractMigration
 
     public function up(Schema $schema): void
     {
+        $this->addSql('DROP TABLE IF EXISTS guitar_tabs');
         // this up() migration is auto-generated, please modify it to your needs
         $this->addSql('CREATE TABLE guitar_tab (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, name VARCHAR(255) NOT NULL, artist VARCHAR(255) DEFAULT NULL, capo SMALLINT NOT NULL, uploaded_at DATETIME NOT NULL --(DC2Type:datetime_immutable)
         , content CLOB NOT NULL)');
-        $this->addSql('DROP TABLE guitar_tabs');
     }
 
     public function down(Schema $schema): void
